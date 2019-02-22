205ae058021f4d6ea0f6ecbbdc2eceb1991d9341
Tom Spencer <tomspencer@toms-air.home>
2019-02-22 09:12:52 +0000

    Modify and style the Home page

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe StaticPagesController, type: :controller do
+RSpec.describe StaticPagesController, type: :request do
 
   describe "GET #home" do
+
     it "returns home page" do
-      get :home
+      get root_path
       expect(response).to have_http_status(:success)
       assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
     end
@@ -12,7 +13,7 @@ RSpec.describe StaticPagesController, type: :controller do
 
   describe "GET #help" do
     it "returns help page" do
-      get :help
+      get static_pages_help_url
       expect(response).to have_http_status(:success)
       assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
     end
@@ -20,7 +21,7 @@ RSpec.describe StaticPagesController, type: :controller do
 
   describe "GET #about" do
     it "returns about page" do
-      get :about
+      get static_pages_about_url
       expect(response).to have_http_status(:success)
       assert_select "title", "About | Ruby on Rails Tutorial Sample App"
     end
