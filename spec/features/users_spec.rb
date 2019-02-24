9d0b100d819b4f981725d8400215d1d9e1ca4178
Tom Spencer <tomspencer@toms-air.home>
2019-02-24 17:15:19 +0000

    Add user signup with specs and message on application.html.erb

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,14 +1,37 @@
 require 'rails_helper'
 
-RSpec.feature "Users", type: :feature do
+RSpec.feature "UserSignup", type: :feature do
 
-  # before(:each) do
-  #   Capybara.app_host = 'https://shrouded-ravine-12833.herokuapp.com'
-  # end
+  before(:each) do
+    Capybara.app_host = 'https://www.tomsampleapp.com'
+  end
 
-  scenario 'visiting the home page' do
-    visit root_path
-    expect(current_path).to eq("/")
-    expect(page).to have_content 'Welcome to the Sample App'
+  scenario 'visit signup' do
+    visit signup_path
+    expect(current_path).to eq("/signup")
+  end
+
+  scenario 'invalid signup information' do
+    user_count = User.count
+    visit signup_path
+    fill_in 'user[name]', with: '   '
+    fill_in 'user[email]', with: 'tom@spencer.co.uk'
+    fill_in 'user[password]', with: 'dud'
+    fill_in 'user[password_confirmation]', with: 'dud'
+    find('input[name="commit"]').click
+    expect(User.count).to eq(user_count)
+  end
+
+  scenario 'valid signup information' do
+    user_count = User.count
+    new_count = user_count + 1
+    visit signup_path
+    expect(current_path).to eq("/signup")
+    fill_in 'user[name]', with: 'Tom Spencer'
+    fill_in 'user[email]', with: 'tom@spencer.co.uk'
+    fill_in 'user[password]', with: 'Password'
+    fill_in 'user[password_confirmation]', with: 'Password'
+    find('input[name="commit"]').click
+    expect(User.count).to eq(new_count)
   end
 end
