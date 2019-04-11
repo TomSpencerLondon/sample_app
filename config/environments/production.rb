43278207a995297e9e5831f7c6677218f5693c7e
Tom Spencer <tomspencer@toms-air.home>
2019-04-11 16:37:21 +0100

    Add factorybot for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,4 +104,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.include FactoryBot::Syntax::Methods
+  config.include TestPasswordHelper
 end
